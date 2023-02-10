df81db8969dbb6bc62ced8917cee5e30d4adb3ce
Kauan-Azevedo <kauan.azevedo@fontespromotora.com.br>
2023-02-10 14:57:04 -0300

    feat: create method

--- js/text-inside.js
+++ js/text-inside.js
@@ -4,7 +4,29 @@ class Nodes {
     return document.getElementById(id);
   }
 
+  create(id, text, nodes) {
+    let el = this.element(id);
+    let node = document.createElement('div');
+    let txt = document.createTextNode(text);
+    node.style.width = '50px';
+    node.style.height = '50px';
+    node.style.border = '2px solid black';
+    node.style.borderRadius = '100%';
+    node.style.textAlign = 'center';
+    node.style.padding = '10px 5px';
 
+    node.style.position = 'absolute';
+    for (let i = 0; i <= nodes; i++) {
+      let posX = Math.floor(Math.random() * window.innerWidth);
+      let posY = Math.floor(Math.random() * window.innerHeight);
+
+      node.style.top = `${posX}px`;
+      node.style.left = `${posY}px`;
+    }
+
+    node.appendChild(txt);
+    el.appendChild(node);
+  }
 
 
 }
